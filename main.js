ab93d9933777a055ddb258e7f9bcf2b02b9a6942
David S <dvdsanar@gmail.com>
2022-06-04 10:34:46 +0200

    feat: game routes at main to use

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = express();
 const dbconect = require("./config/connection.js");
 const dotenvProject = require("dotenv");
 const usRoutes = require("./users/usRoutes.js");
+const gameRoutes = require("./games/gameRoutes");
 
 dotenvProject.config();
 
@@ -13,3 +14,4 @@ app.use(express.json());
 app.listen(process.env.PORT, () => console.log("Succes!! Server is running"));
 
 app.use("/users", usRoutes);
+app.use("/games", gameRoutes);
