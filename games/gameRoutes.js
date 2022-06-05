5789455e4adc1edb23032e3447999838fbe3dc4d
David S <dvdsanar@gmail.com>
2022-06-05 19:50:18 +0200

    feat: add middleware to all routes that need

--- games/gameRoutes.js
+++ games/gameRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./gameController.js");
+const verification = require("../config/middleware.js");
 
-router.get("/", controller.getGames);
-router.get("/:id", controller.getGameId);
-router.post("/", controller.newGame);
+router.get("/", verification(), controller.getGames);
+router.get("/:id", verification(), controller.getGameId);
+router.post("/", verification(), controller.newGame);
 // router.patch("/:id", controller.patchGame);
 // router.delete("/:id", controller.deleteGame);
 
--- users/usRoutes.js
+++ users/usRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./usController.js");
+const verification = require("../config/middleware.js");
 
-router.get("/", controller.getUsers);
-router.get("/:id", controller.getUserId);
+router.get("/", verification("admin"), controller.getUsers);
+router.get("/:id", verification(), controller.getUserId);
 router.post("/", controller.newUser);
-router.patch("/:id", controller.patchUser);
-router.delete("/:id", controller.deleteUser);
-router.patch("/card/:id", controller.addCard);
-router.patch("/balance/:id", controller.addBalance);
+router.patch("/:id", verification(), controller.patchUser);
+router.delete("/:id", verification("admin"), controller.deleteUser);
+router.patch("/card/:id", verification(), controller.addCard);
+router.patch("/balance/:id", verification(), controller.addBalance);
 
 router.post("/login", controller.getToken);
 
