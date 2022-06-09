9297e244f3b20095e18d53b338b778496f3c1efb
David S <dvdsanar@gmail.com>
2022-06-09 20:01:55 +0200

    feat: add cors to can use frontend

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const dbconect = require("./config/connection.js");
 const dotenvProject = require("dotenv");
 const usRoutes = require("./users/usRoutes.js");
 const gameRoutes = require("./games/gameRoutes");
+const cors = require("cors");
 
 dotenvProject.config();
 
 dbconect();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.PORT, () => console.log("Succes!! Server is running"));
 
